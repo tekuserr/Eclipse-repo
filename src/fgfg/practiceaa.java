3f3968da6ff1ec1cb05e0d98c7a0fc8c1ddc62ac
tekuserr <117806805+tekuserr@users.noreply.github.com>
2022-11-11 19:33:26 -0500

    Update practiceaa.java

--- src/fgfg/practiceaa.java
+++ src/fgfg/practiceaa.java
@@ -5,6 +5,7 @@ public class practiceaa {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 System.out.println("fhgfhgfhgf");
+        System.out.println("hghgjghgjhghjhghjg");
     }
 
 }
